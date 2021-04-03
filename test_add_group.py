3ebd3a07590d99d405d5f33fcc34b7dfbf5d77c2
monika-goszczynska <monika.gx@gmail.com>
2021-04-03 20:51:48 +0200

    Moved from unittest to pytest CORRECTED TASK 4

--- test_add_group.py
+++ test_add_group.py
@@ -4,22 +4,21 @@ import pytest
 from group import Group
 from application import Application
 
+"""@... zamienia zwykłą funkcję na inicjalizator fikstury"""
+@pytest.fixture()
+def app(request):
+    fixture = Application
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class TestAddGroup(unittest.TestCase):
 
-    """@... zamienia zwykłą funkcję na inicjalizator fikstury"""
-    @pytest.fixture()
-    def app(request):
-        fixture=Application
-        request.addfinalizer(fixture.destroy)
-        return fixture
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="gr_name", header="gr_header", footer="gr_footer"))
+    app.logout()
 
-    def test_add_group(app):
-        app.login(username="admin", password="secret")
-        app.create_group(Group(name="gr_name", header="gr_header", footer="gr_footer"))
-        app.logout()
 
-    def test_add_empty_group(app):
-        app.login(username="admin", password="secret")
-        app.create_group(Group(name="", header="", footer=""))
-        app.logout()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()

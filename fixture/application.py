b8064116e4a8b209554af12f34169b7e5d7009f8
monika-goszczynska <monika.gx@gmail.com>
2021-04-06 18:54:23 +0200

    address part decomposed TASK 5

--- fixture/application.py
+++ fixture/application.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.address import AddressHelper
 
 
 class Application:
@@ -13,72 +13,11 @@ class Application:
         # pomocnik uzyskuje odsyłacz do obiektu klasy Application
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.address = AddressHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost:8080/addressbook/")
-
-
-    def add_new_address(self, address):
-        # to jest łącze do sterownika webdriver.Firefox
-        wd = self.wd
-        # init address creation
-        wd.find_element_by_link_text("add new").click()
-        # fill in address form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(address.first_name)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(address.middle_name)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(address.last_name)
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(address.nickname)
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(address.title)
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(address.company)
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address.address)
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(address.home_telephone)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(address.mobile_telephone)
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(address.work_telephone)
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(address.fax)
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(address.email)
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(address.email2)
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(address.email3)
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(address.homepage)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(address.birth_day)
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(address.birth_month)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(address.birth_year)
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(address.anni_day)
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(address.anni_month)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(address.anni_year)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(address.second_address)
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(address.home)
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(address.notes)
-        # submit address creation
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_address.py
+++ test/test_add_address.py
@@ -13,16 +13,16 @@ def app(request):
 
 
 def test_add_address(app):
-    app.login(username="admin", password="secret")
-    app.add_new_address(Address(first_name="fname", middle_name="mname", last_name="lname", nickname="nickname", title="title", company="company", address="address", home_telephone="hometel",
-                         mobile_telephone="mobtel", work_telephone="worktel", fax="faxtel", email="email@email.com", email2="email2@email.com", email3="email3@email.com",
-                         homepage="homepage", birth_day="1", birth_month="January", birth_year="1990", anni_day="1", anni_month="January", anni_year="2000", second_address="secaddress", home="home", notes="notes"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.address.new(Address(first_name="fname", middle_name="mname", last_name="lname", nickname="nickname", title="title", company="company", address="address", home_telephone="hometel",
+                            mobile_telephone="mobtel", work_telephone="worktel", fax="faxtel", email="email@email.com", email2="email2@email.com", email3="email3@email.com",
+                            homepage="homepage", birth_day="1", birth_month="January", birth_year="1990", anni_day="1", anni_month="January", anni_year="2000", second_address="secaddress", home="home", notes="notes"))
+    app.session.logout()
 
 
 def test_add_empty_address(app):
-    app.login(username="admin", password="secret")
-    app.add_new_address(Address(first_name="", middle_name="", last_name="", nickname="", title="", company="", address="", home_telephone="",
-                         mobile_telephone="", work_telephone="", fax="", email="", email2="", email3="",
-                         homepage="", birth_day="", birth_month="-", birth_year="", anni_day="", anni_month="-", anni_year="", second_address="", home="", notes=""))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.address.new(Address(first_name="", middle_name="", last_name="", nickname="", title="", company="", address="", home_telephone="",
+                            mobile_telephone="", work_telephone="", fax="", email="", email2="", email3="",
+                            homepage="", birth_day="", birth_month="-", birth_year="", anni_day="", anni_month="-", anni_year="", second_address="", home="", notes=""))
+    app.session.logout()

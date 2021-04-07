661919698876fe5b625944a1916df1f342303f65
monika-goszczynska <monika.gx@gmail.com>
2021-04-07 20:43:02 +0200

    modify group method

--- fixture/group.py
+++ fixture/group.py
@@ -34,6 +34,18 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
+    def modify_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # modify group
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").send_keys('edit')
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
---
+++ test/test_modify_group.py
@@ -0,0 +1,5 @@
+
+def test_modify_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group()
+    app.session.logout()

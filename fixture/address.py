8372e2a33a934969354269196d7edee655165378
monika-goszczynska <monika.gx@gmail.com>
2021-04-09 21:26:45 +0200

    modify_first_address test with if statement with date issue

--- fixture/address.py
+++ fixture/address.py
@@ -6,76 +6,80 @@ class AddressHelper:
     def __init__(self, app):
         self.app = app
 
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_address_form(self, address):
+        wd = self.app.wd
+        self.change_field_value("firstname", address.first_name)
+        self.change_field_value("middlename", address.middle_name)
+        self.change_field_value("lastname", address.last_name)
+        self.change_field_value("nickname", address.nickname)
+        self.change_field_value("title", address.title)
+        self.change_field_value("company", address.company)
+        self.change_field_value("address", address.address)
+        self.change_field_value("home", address.home)
+        self.change_field_value("mobile", address.mobile_telephone)
+        self.change_field_value("work", address.work_telephone)
+        self.change_field_value("fax", address.fax)
+        self.change_field_value("email", address.email)
+        self.change_field_value("email2", address.email2)
+        self.change_field_value("email3", address.email3)
+        self.change_field_value("homepage", address.homepage)
+        # change all dates!!!
+        self.change_field_value("bday", address.birth_day)
+        self.change_field_value("bmonth", address.birth_month)
+        self.change_field_value("byear", address.birth_year)
+        self.change_field_value("aday", address.anni_day)
+        self.change_field_value("amonth", address.anni_month)
+        self.change_field_value("ayear", address.anni_year)
+        self.change_field_value("address2", address.second_address)
+        self.change_field_value("phone2", address.home)
+        self.change_field_value("notes", address.notes)
+        # wd.find_element_by_name("bday").click()
+        # Select(wd.find_element_by_name("bday")).select_by_visible_text(address.birth_day)
+        # wd.find_element_by_name("bmonth").click()
+        # Select(wd.find_element_by_name("bmonth")).select_by_visible_text(address.birth_month)
+        # wd.find_element_by_name("byear").click()
+        # wd.find_element_by_name("byear").clear()
+        # wd.find_element_by_name("byear").send_keys(address.birth_year)
+        # wd.find_element_by_name("aday").click()
+        # Select(wd.find_element_by_name("aday")).select_by_visible_text(address.anni_day)
+        # wd.find_element_by_name("amonth").click()
+        # Select(wd.find_element_by_name("amonth")).select_by_visible_text(address.anni_month)
+        # wd.find_element_by_name("ayear").click()
+        # wd.find_element_by_name("ayear").clear()
+        # wd.find_element_by_name("ayear").send_keys(address.anni_year)
+
     def new(self, address):
         # to jest łącze do sterownika webdriver.Firefox
         wd = self.app.wd
         # init address creation
         wd.find_element_by_link_text("add new").click()
-        # fill in address form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(address.first_name)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(address.middle_name)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(address.last_name)
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(address.nickname)
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(address.title)
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(address.company)
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address.address)
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(address.home_telephone)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(address.mobile_telephone)
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(address.work_telephone)
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(address.fax)
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(address.email)
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(address.email2)
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(address.email3)
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(address.homepage)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(address.birth_day)
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(address.birth_month)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(address.birth_year)
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(address.anni_day)
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(address.anni_month)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(address.anni_year)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(address.second_address)
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(address.home)
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(address.notes)
+        self.fill_address_form(address)
         # submit address creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-    def delete_first_address(self):
+    def select_first_address(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
+
+    def delete_first_address(self):
+        wd = self.app.wd
+        self.select_first_address()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
 
-    def modify_first_address(self):
+    def modify_first_address(self, new_address_data):
         wd = self.app.wd
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_address()
+        # open modification form
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").send_keys('modified')
+        self.fill_address_form(new_address_data)
+        # submit modification
+        wd.find_element_by_name("update").click()
+        # wd.switch_to_alert().accept()
--- test/test_modify_address.py
+++ test/test_modify_address.py
@@ -1,5 +1,12 @@
+from model.address import Address
 
-def test_modify_first_address(app):
+
+def test_modify_first_name(app):
     app.session.login(username="admin", password="secret")
-    app.address.modify_first_address()
+    app.address.modify_first_address(Address(first_name="New first name"))
+    app.session.logout()
+
+def test_modify_bday(app):
+    app.session.login(username="admin", password="secret")
+    app.address.modify_first_address(Address(birth_day="20"))
     app.session.logout()

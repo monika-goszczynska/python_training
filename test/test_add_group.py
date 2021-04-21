329bb954272b88141d5cf95bec95b08fdf56a481
monika-goszczynska <monika.gx@gmail.com>
2021-04-21 20:50:43 +0200

    cache&hash test add group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,19 +6,19 @@ def test_add_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="gr_name", header="gr_header", footer="gr_footer")
     app.group.create(group)
-    new_groups = app.group.get_group_list()
     # weryfikacja w tescie
-    assert len(old_groups) + 1 == len(new_groups)
+    assert len(old_groups) + 1 == app.group.count()
+    new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
+#def test_add_empty_group(app):
+#    old_groups = app.group.get_group_list()
+#    group = Group(name="", header="", footer="")
+#    app.group.create(group)
     # weryfikacja w tescie
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+#    assert len(old_groups) + 1 == app.group.count()
+#    new_groups = app.group.get_group_list()
+#    old_groups.append(group)
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)

e2cc5e84638dc6a54a6072b3d7935a7b74a6570b
monika-goszczynska <monika.gx@gmail.com>
2021-04-28 12:27:02 +0200

    Data Driven Testing adding groups and contacts

--- test/test_add_address.py
+++ test/test_add_address.py
@@ -1,41 +1,32 @@
 # -*- coding: utf-8 -*-
 from model.address import Address
 import pytest
+import random
+import string
 
 
 def random_string(prefix, maxlen):
-    symbols = string.digits + string.ascii_letters
+    symbols = string.digits + string.ascii_letters + string.punctuation + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
 testdata = [Address(first_name="", middle_name="", last_name="", nickname="", title="", company="", address="", home_telephone="", mobile_telephone="",
                     work_telephone="", fax="", email="", email2="", email3="", homepage="", birth_day="", birth_month="-", birth_year="", anni_day="",
                     anni_month="-", anni_year="", second_address="", phone2="", notes="")] + [
-    Address(first_name=random_string("fname", 10), middle_name=random_string("mname", 10), last_name=random_string("lname", 10), nickname=random_string("nick", 10),
-            title=random_string("title", 10), company=random_string("comp", 10), address=random_string("address", 10), home_telephone=random_string("htel", 10),
-            mobile_telephone=random_string("mtel", 10), work_telephone=random_string("wtel", 10), fax=random_string("fax", 10), email=random_string("email@", 5),
-            email2=random_string("email2@", 5), email3=random_string("email3@", 5), homepage=random_string("homep", 10), birth_day="1", birth_month="January", birth_year="1990", anni_day="1", anni_month="January",
-                    anni_year="2000", second_address="secaddress", phone2="home", notes="notes")
+    Address(first_name=random_string("fname", 10), middle_name=random_string("mname", 10), last_name=random_string("lname", 10), nickname=random_string("nick", 5),
+            title=random_string("title", 5), company=random_string("comp", 5), address=random_string("address", 10), home_telephone=random_string("htel", 5),
+            mobile_telephone=random_string("mtel", 5), work_telephone=random_string("wtel", 5), fax=random_string("fax", 5), email=random_string("email@", 5),
+            email2=random_string("email2@", 5), email3=random_string("email3@", 5), homepage=random_string("homep", 10), birth_day="1", birth_month="January",
+            birth_year="1990", anni_day="1", anni_month="January", anni_year="2000", second_address=random_string("secaddr", 10), phone2=random_string("phone2", 5),
+            notes=random_string("notes", 10))
 ]
 
 
-def test_add_address(app):
+@pytest.mark.parametrize("address", testdata, ids=[repr(x) for x in testdata])
+def test_add_address(app, address):
     old_addresses = app.address.get_address_list()
     app.address.new(address)
     assert len(old_addresses) + 1 == app.address.count()
     new_addresses = app.address.get_address_list()
     old_addresses.append(address)
     assert sorted(old_addresses, key=Address.id_or_max) == sorted(new_addresses, key=Address.id_or_max)
-
-
-
-#def test_add_empty_address(app):
-#    old_addresses = app.address.get_address_list()
-#    address = Address(first_name="", middle_name="", last_name="", nickname="", title="", company="", address="", home_telephone="",
-#                            mobile_telephone="", work_telephone="", fax="", email="", email2="", email3="",
-#                            homepage="", birth_day="", birth_month="-", birth_year="", anni_day="", anni_month="-", anni_year="", second_address="", phone2="", notes="")
-#    app.address.new(address)
-#    new_addresses = app.address.get_address_list()
-#    assert len(old_addresses) + 1 == len(new_addresses)
-#    old_addresses.append(address)
-#    assert sorted(old_addresses, key=Address.id_or_max) == sorted(new_addresses, key=Address.id_or_max)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -25,14 +25,3 @@ def test_add_group(app, group):
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-# def test_add_empty_group(app):
-#    old_groups = app.group.get_group_list()
-#    group = Group(name="", header="", footer="")
-#    app.group.create(group)
-    # weryfikacja w tescie
-#    assert len(old_groups) + 1 == app.group.count()
-#    new_groups = app.group.get_group_list()
-#    old_groups.append(group)
-#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)

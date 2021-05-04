f4dc6da940742773922496530c336702eafd263d
monika-goszczynska <monika.gx@gmail.com>
2021-05-04 10:59:19 +0200

    confguration file load

--- conftest.py
+++ conftest.py
@@ -1,22 +1,24 @@
 import pytest
+import json
 from fixture.application import Application
 
 
 fixture = None
+target = None
 
 
 # inicjalizator fikstury
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target["baseUrl"])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 
@@ -33,4 +35,4 @@ def stop(request):
 def pytest_addoption(parser):
     # przekazywany bedzie parser wiersza polecen w ktorym jest metoda addoption
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost:8080/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")

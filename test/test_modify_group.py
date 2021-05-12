766f57077ae2d2efca6297a184b53d8dbd780a9d
monika-goszczynska <monika.gx@gmail.com>
2021-05-12 18:46:07 +0200

    test_modify_group.py AssertionError

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,23 +3,15 @@ from random import randrange
 
 
 def test_modify_group_name(app, db, check_ui):
-    #jesli pusto tworzymy grupe
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test", header="test", footer="test"))
-    # zapisujemy grupe przed modyf z bazy danych
     old_groups = db.get_group_list()
-    #wybieramy losowy element z grupy przed modyf.
     id = randrange(0, len(old_groups))
     group = old_groups[id]
-    # modyfikujemy grupe
-    app.group.modify_group_by_id(group.id, Group(name="newname", header="newheader", footer="newfooter"))
-    # zapisujemy grupe po modyf z bazy danych
-    new_groups = db.get_group_list()
-    # porownujemy dlugosc list
-    assert len(old_groups) == len(new_groups)
-    # zastepujemy stara grupe zmodyfikowana
+    app.group.modify_group_by_id(group.id, Group(name="NewName", header="NewHeader", footer="NewFooter"))
     old_groups[id] = group
-    # poronujemy stara i nowa grupe
+    new_groups = db.get_group_list()
+    assert len(old_groups) == len(new_groups)
     assert old_groups == new_groups
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
